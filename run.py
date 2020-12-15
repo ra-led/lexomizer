b44cb35d49d8d68f798c3a48e531897a2f63176a
Yura Tomakov <yuratomakov@MacBook-Pro-Yura.local>
2020-12-15 17:35:47 +0300

    add pynist config #9

--- run.py
+++ run.py
@@ -2,10 +2,14 @@
 from conf import CFG
 from app import app
 
-if __name__ == '__main__':
-    # run app
+def main():
     app.debug = True
     host = '0.0.0.0'
     port = CFG['port']
 
     app.run(host=host, port=port)
+
+
+if __name__ == '__main__':
+    # run app
+    main()
